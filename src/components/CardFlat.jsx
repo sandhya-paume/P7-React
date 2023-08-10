a476f76bdad7570eb7ef513854c396c68238f1a9
sandhya-paume <sandhya.paume@gmail.com>
2023-08-10 21:24:42 +0200

    modif 10-08

--- src/components/CardFlat.jsx
+++ src/components/CardFlat.jsx
@@ -13,8 +13,7 @@ function CardFlatGen(image, title) {
 function CardFlat()
 {
   return (
-CardFlatGen("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg","Appartement cosy"),
-CardFlatGen("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-1-1.jpg","Magnifique appartement proche Canal Saint Martin")
+CardFlatGen("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg","Appartement cosy")
 );
 };
 
