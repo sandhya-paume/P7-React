e5bde60a91b0281b68adb148c5ac669309400420
sandhya-paume <sandhya.paume@gmail.com>
2023-09-27 23:46:15 +0200

    mise a jour du projet + finalisation du responsive

--- src/Pages/Accommodation_Sheet/Index/ContainerText.jsx
+++ src/Pages/Accommodation_Sheet/Index/ContainerText.jsx
@@ -6,13 +6,15 @@ export default function ContainerText({description, equipments}) {
     return (
 <div>       
     <div className="details">
+        <div className="CollapseBox">
         <Collapse title="Description"
          description={description} />
-    <div className="Spacer"></div>
+    
+         <div className="Spacer">&nbsp;</div>
          <Collapse title="Equipements"
          description={equipments}
          equipments={1}/>
-    </div>
+    </div></div>
 </div> 
     )
 }
--- src/Pages/Accommodation_Sheet/Index/Slideshow.jsx
+++ src/Pages/Accommodation_Sheet/Index/Slideshow.jsx
@@ -16,7 +16,7 @@ const Slideshow = ({ images }) => { //La fonction du composant "Carrousel" qui p
   return (
     <div className="carrousel-container">
       <button className="arrow-button prev-button" onClick={prevSlide}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
+      <svg className="arrows" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 80" fill="none">
         <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
       </svg> 
       </button>
@@ -26,7 +26,7 @@ const Slideshow = ({ images }) => { //La fonction du composant "Carrousel" qui p
           <div className="pagination">{currentIndex +1}/{images.length}</div> 
 
       <button className="arrow-button next-button" onClick={nextSlide}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
+      <svg xmlns="http://www.w3.org/2000/svg" className="arrows" viewBox="0 0 48 80" fill="none">
         <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/>
       </svg>
       </button>
